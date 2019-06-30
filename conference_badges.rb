79f3b6df8ac22a9a38c3267a941a900d87ab4e27
elegant-intelligence-1985 <cp.halpenny@gmail.com>
2019-06-30 01:53:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,14 +3,17 @@ def badge_maker(speaker)
 end
 
 
-  def batch_badge_creator(attendees)
+def batch_badge_creator(attendees)
    #create new array to put name inside
    array = []
    #loop through each name provided and push the message to new array with the name
    attendees.each { |name| array << badge_maker(name) }
    array #return the new array
 end
-
-
 # what is the return value of the method?
   #an array of strings
+
+def assign_rooms(guest_list)
+  room_assignments = []
+  guest_list.each_with_index { |room| room_assignments << "Hello, #{guest_list}! You'll be assigned to room #{}!" }
+end 
